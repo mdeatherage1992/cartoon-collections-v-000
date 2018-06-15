6df06728a3d24fca695d75e698b1a96c74841837
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-15 18:43:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,13 @@ def long_planeteer_calls(array)
   return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |element|
+    if cheese_types.include(element)
+      return element
+    else
+      nil
+    end
+  end
 end
