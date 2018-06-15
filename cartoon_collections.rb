66ebe9209238d9acfb6cde443782a29a975c2792
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-15 18:34:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,12 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.all? {|phrase| !!(phrase.length < 4)}
+  array.all? do |phrase|
+    if phrase.length < 4
+      true
+    else
+      false
+    end
 end
 
 def find_the_cheese# code an argument here
